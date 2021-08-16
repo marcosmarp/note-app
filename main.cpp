12945408bd5f9b3bc9789bf546dec057d2984d1a
Marcos <88422056+marcosmarp@users.noreply.github.com>
2021-08-16 03:45:46 -0300

    Added edit note option (#4)

--- main.cpp
+++ main.cpp
@@ -22,7 +22,11 @@ int main() {
         break;
       }
       case MainMenuOptions::kDisplayNotes: {
-        DisplayNotes(notes_vector);
+        DisplayNotes(notes_vector, true);
+        break;
+      }
+      case MainMenuOptions::kEditNote: {
+        EditNote(notes_vector);
         break;
       }
       case MainMenuOptions::kExit: {
@@ -222,15 +226,220 @@ void NewNote(std::vector<NotesData>& stored_notes) {
   return;
 }
 
-void DisplayNotes(std::vector<NotesData>& stored_notes) {
-  ClearScreen();
+bool DisplayNotes(std::vector<NotesData>& stored_notes, bool calling_from_main) {
+  /* This function can be called from the main menu or from the Edit note sector, in this case
+  we dont want to delete the Edit note intro */
+  if (calling_from_main == true) ClearScreen();
   std::cout << "STORED NOTES" << std::endl << std::endl;
-  for(auto& it : stored_notes) {
-    std::cout << "  Title: " << it.title << std::endl;
-    std::cout << "  Content: " << it.content << std::endl << std::endl;
+  if(stored_notes.size() == 0) {
+    std::cout << "You don't have any notes. You can create one in the main menu" << std::endl << std::endl;
+    std::cout << "Press any key to return to main menu";
+    _getch();
+    return false;
+  } 
+  else {
+    for(auto& it : stored_notes) {
+      std::cout << "  Title: " << it.title << std::endl;
+      std::cout << "  Content: " << it.content << std::endl << std::endl;
+    }
   }
-  std::cout << "Press any key to return to main menu";
-  _getch();
+  if(calling_from_main == true) {
+    std::cout << "Press any key to return to main menu";
+    _getch();
+  }
+  return true;
+}
+
+void EditNote(std::vector<NotesData>& stored_notes) {
+  auto user_option{EditOptions::kEditTitle};
+  while (user_option != EditOptions::kExitToMainMenu) {
+    ClearScreen();
+    std::cout << "THIS ARE YOUR ";
+    if(DisplayNotes(stored_notes, false) == false) {
+      return;
+    }
+    std::string title_to_edit;
+    auto note_to_edit = SearchForNoteByTitle(title_to_edit, stored_notes);
+    while(note_to_edit == stored_notes.end()) {
+      std::cout << "Enter the title of the note to edit [tipe 'exit' to return to main menu]: ";
+      getline(std::cin, title_to_edit);
+      std::cout << std::endl;
+      if(title_to_edit == "exit") return;
+      note_to_edit = SearchForNoteByTitle(title_to_edit, stored_notes);
+      if(note_to_edit == stored_notes.end()) std::cout << "Inexistent title, try again. ";
+    }
+
+    user_option = ScrollEditMenu(title_to_edit);
+    switch(user_option) {
+      case EditOptions::kEditTitle: {
+        EditTitle(note_to_edit->title, title_to_edit);
+        break;
+      }
+      case EditOptions::kEditContent: {
+        EditContent(note_to_edit->content, title_to_edit);
+        break;
+      }
+    }
+  }
+  PrintNotesInTxt(stored_notes);
+  return;
+}
+
+void PrintNotesInTxt(const std::vector<NotesData>& notes) {
+  std::ofstream txt_file;
+  txt_file.open("notes.txt");
+  for(auto& it : notes) {
+    txt_file << it.title << std::endl;
+    txt_file << it.content << std::endl;
+  }
+  txt_file.close();
+  return;
+}
+
+void EditTitle(std::string& title, std::string& original_title) {
+  ClearScreen();
+  std::cout << "NOTE '" << original_title << "'" << std::endl << std::endl;
+  std::cout << "Enter new title: ";
+  getline(std::cin, title);
+  std::cout << std::endl;
+  original_title = title;
+  return;
+}
+
+void EditContent(std::string& content, const std::string title) {
+  ClearScreen();
+  std::cout << "NOTE '" << title << "'" << std::endl << std::endl;
+  std::cout <<"Enter new content: ";
+  getline(std::cin, content);
+  std::cout << std::endl;
+  return;
+}
+
+std::vector<NotesData>::iterator SearchForNoteByTitle(const std::string title, std::vector<NotesData>& notes) {
+  auto it = notes.begin();
+  while(it != notes.end()) {
+    if(it->title == title) break;
+    ++it;
+  }
+  return it;
+}
+
+EditOptions ScrollEditMenu(const std::string title) {
+  auto last_item_scrolled{EditOptions::kEditTitle};
+  PrintEditMenuHighlighted(EditOptions::kEditTitle, title);
+  while(true) {
+    /* Here we have two switch statements, with two more inside them: basically, the current position of the user in the main menu (first switch with 4 options)
+    and the two possible new positions after the user enters the UP arrow, the DOWN arrow or the enter for going to the selected process */
+    switch(last_item_scrolled) {
+      case EditOptions::kEditTitle: {
+        switch(_getch()) {
+          case ARROW_UP: {
+            PrintEditMenuHighlighted(EditOptions::kExitToMainMenu, title);
+            last_item_scrolled = EditOptions::kExitToMainMenu;
+            break;
+          }
+          case ARROW_DOWN: {
+            PrintEditMenuHighlighted(EditOptions::kEditContent, title);
+            last_item_scrolled = EditOptions::kEditContent;
+            break;
+          }
+          case ENTER_KEY: {
+            return last_item_scrolled;
+          }
+        }
+        break;
+      }
+      case EditOptions::kEditContent: {
+        switch(_getch()) {
+          case ARROW_UP: {
+            PrintEditMenuHighlighted(EditOptions::kEditTitle, title);
+            last_item_scrolled = EditOptions::kEditTitle;
+            break;
+          }
+          case ARROW_DOWN: {
+            PrintEditMenuHighlighted(EditOptions::kExitToEditScreen, title);
+            last_item_scrolled = EditOptions::kExitToEditScreen;
+            break;
+          }
+          case ENTER_KEY: {
+            return last_item_scrolled;
+          }
+        }
+        break;
+      }
+      case EditOptions::kExitToEditScreen: {
+        switch(_getch()) {
+          case ARROW_UP: {
+            PrintEditMenuHighlighted(EditOptions::kEditContent, title);
+            last_item_scrolled = EditOptions::kEditContent;
+            break;
+          }
+          case ARROW_DOWN: {
+            PrintEditMenuHighlighted(EditOptions::kExitToMainMenu, title);
+            last_item_scrolled = EditOptions::kExitToMainMenu;
+            break;
+          }
+          case ENTER_KEY: {
+            return last_item_scrolled;
+          }
+        }
+        break;
+      }
+      case EditOptions::kExitToMainMenu: {
+        switch(_getch()) {
+          case ARROW_UP: {
+            PrintEditMenuHighlighted(EditOptions::kExitToEditScreen, title);
+            last_item_scrolled = EditOptions::kExitToEditScreen;
+            break;
+          }
+          case ARROW_DOWN: {
+            PrintEditMenuHighlighted(EditOptions::kEditTitle, title);
+            last_item_scrolled = EditOptions::kEditTitle;
+            break;
+          }
+          case ENTER_KEY: {
+            return last_item_scrolled;
+          }
+        }
+        break;
+      }
+    }
+  }
+}
+
+void PrintEditMenuHighlighted(const EditOptions highlighted_option, const std::string note_title) {
+  ClearScreen();
+  std::cout << "NOTE '" << note_title << "'" << std::endl << std::endl;
+  switch(highlighted_option) {
+    case EditOptions::kEditTitle: {
+      std::cout << "[Edit title]" << std::endl << std::endl;
+      std::cout << "Edit content" << std::endl << std::endl;
+      std::cout << "Return to edit menu" << std::endl << std::endl;
+      std::cout << "Return to main menu" << std::endl << std::endl;
+      break;
+    }
+    case EditOptions::kEditContent: {
+      std::cout << "Edit title" << std::endl << std::endl;
+      std::cout << "[Edit content]" << std::endl << std::endl;
+      std::cout << "Return to edit menu" << std::endl << std::endl;
+      std::cout << "Return to main menu" << std::endl << std::endl;
+      break;
+    }
+    case EditOptions::kExitToEditScreen: {
+      std::cout << "Edit title" << std::endl << std::endl;
+      std::cout << "Edit content" << std::endl << std::endl;
+      std::cout << "[Return to edit menu]" << std::endl << std::endl;
+      std::cout << "Return to main menu" << std::endl << std::endl;
+      break;
+    }
+    case EditOptions::kExitToMainMenu: {
+      std::cout << "Edit title" << std::endl << std::endl;
+      std::cout << "Edit content" << std::endl << std::endl;
+      std::cout << "Return to edit menu" << std::endl << std::endl;
+      std::cout << "[Return to main menu]" << std::endl << std::endl;
+      break;
+    }
+  }
   return;
 }
 
